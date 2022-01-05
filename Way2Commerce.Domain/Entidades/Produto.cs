ce2e44cba6667d92afcfd34e3ea94654463eb7ea
Warley Lopes <warley.thales0@gmail.com>
2022-01-05 00:47:14 -0300

    Iniciando criação do dominio

---
+++ Way2Commerce.Domain/Entidades/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace Way2Commerce.Domain.Entidades
+{
+    public  class Produto
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataHoraCadastro { get; set; }
+        public int CategoriaId { get; set; }
+        public CategoriaProduto Categoria { get; set; }
+    }
+}
---
+++ Way2Commerce.Domain/Repositorios/IProdutoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using Way2Commerce.Domain.Entidades;
+
+namespace Way2Commerce.Domain.Repositorios
+{
+    public interface IProdutoRepositorio
+    {
+        IList<Produto> BuscarTodos();        
+        int Salvar(Produto produto);
+        void Alterar(Produto produto);
+        void Excluir(int ProdutoId);
+    }
+}
