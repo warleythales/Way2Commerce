b4e32e39160d6c058b26913f45c29bbf015f88f7
Warley Lopes <warley.thales0@gmail.com>
2022-01-06 01:34:31 -0300

    Iniciando implementação da camada API + Contrato com o repositorio

---
+++ Way2Commerce.Data/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using Way2Commerce.Domain.Entidades;
+using Way2Commerce.Domain.Repositorios;
+
+namespace Way2Commerce.Data.Repositorios
+{
+    public class ProdutoRepositorio : IProdutoRepositorio
+    {
+        private Way2CommerceContexto _dbContexto;
+        public ProdutoRepositorio(Way2CommerceContexto dbContexto)
+        {
+            _dbContexto = dbContexto;
+        }
+
+        public void Alterar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Produto> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int ProdutoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Salvar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
